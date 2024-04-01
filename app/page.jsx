8f5298cba695a15844d55b33a414fb02edbb4160
Moises Roberto Sanchez Perez <moisesrobertosanchezperez@gmail.com>
2024-04-01 16:12:43 -0600

    Arreglando error 1

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,11 @@
 "use client"
 import "./page.css";
 
-export default function Home({ children }) {
-  location.href = '/promociones'
+export default function Home() {
   return (
     <>
+      <h1>Ve a promociones</h1>
+      <p>Aquí no hay nada ver</p>
     </>
   );
 }
